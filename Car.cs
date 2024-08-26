27e8d69a7c56f1716e5621d0dd4f71c9cd01590f
Madushani <Madushani@DESKTOP-JV13050>
2024-08-26 12:35:03 +0530

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designPatterns
+{
+    public class Car
+    {
+        private int id {  get; set; }
+        private string brand { get; set; }
+        private string color { get; set; }
+        private string model { get; set; }
+
+        Car(CarBuilder carBuilder)
+        {
+            this.id = carBuilder.id;
+            this.brand = carBuilder.brand;
+            this.color = carBuilder.color;
+            this.model = carBuilder.model;
+        }
+        
+        public void Display()
+            {
+                Console.WriteLine($"{id} {brand} {model}");
+            }
+
+        public class CarBuilder
+        {
+            public int id { get; set; }
+            public string brand { get; set; }
+            public string color { get; set; }
+            public string model { get; set; }
+
+            public CarBuilder Id(int id)
+            {
+                this.id = id;
+                return this;
+            }
+
+            public CarBuilder Brand(String brand)
+            {
+                this.brand = brand;
+                return this;
+            }
+
+            public CarBuilder Color(string color)
+            {
+                this.color = color;
+                return this;
+            }
+            public CarBuilder Model(string model)
+            {
+                this.model = model;
+                return this;
+            }
+
+            public Car Build() 
+            {
+                return new Car(this);
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace designPatterns
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Car car = new Car.CarBuilder().
+              Id(12).Brand("Toyota").Color("red").Model("18/17").Build();
+            car.Display();
+
+            Car car1 = new Car.CarBuilder().
+              Id(20).Brand("Honda").Build();
+            car1.Display();
+
+
+        }
+    }
+}
